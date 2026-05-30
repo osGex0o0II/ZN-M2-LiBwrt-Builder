7165878ad1aebea525bb6e8027a830f6eeb3ed9d
#eaB@Il <108638675+osGex0o0II@users.noreply.github.com>
2026-05-31 04:40:53 +0800

    Update 99-set-ui.sh

--- files/etc/uci-defaults/99-set-ui.sh
+++ files/etc/uci-defaults/99-set-ui.sh
@@ -12,27 +12,13 @@ uci set system.@system[0].hostname='ZN-M2'
 uci set firewall.@defaults[0].flow_offloading='1'
 uci set firewall.@defaults[0].flow_offloading_hw='1'
 
-# 3. 保存并应用配置
+# 3. 修改默认 LAN 口 IP 地址为 10.0.5.1
+uci set network.lan.ipaddr='10.0.5.1'
+
+# 4. 保存并应用所有配置
 uci commit luci
 uci commit system
 uci commit firewall
-
-exit 0#!/bin/sh
-# ==========================================
-# 首次开机初始化脚本 (执行一次后自动销毁)
-# ==========================================
-
-# 1. 强制设置语言为简体中文
-uci set luci.main.lang='zh_cn'
-
-# 2. 强制设置主题为 Aurora
-uci set luci.main.mediaurlbase='/luci-static/aurora'
-
-# 3. 强制设置主机名为 ZN-M2 
-uci set system.@system[0].hostname='ZN-M2'
-
-# 4. 保存并应用配置
-uci commit luci
-uci commit system
+uci commit network
 
 exit 0
