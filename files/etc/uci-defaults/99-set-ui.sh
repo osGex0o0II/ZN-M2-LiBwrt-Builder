512918b1f6d95128e85d039aff35cd31bf6edfed
#eaB@Il <108638675+osGex0o0II@users.noreply.github.com>
2026-05-31 01:34:25 +0800

    Change UI language and theme settings in script

--- files/etc/uci-defaults/99-set-ui.sh
+++ files/etc/uci-defaults/99-set-ui.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-# 首次开机强制设置语言为中文，主题为 Argon
+# 首次开机强制设置语言为中文，主题为 Aurora
 uci set luci.main.lang='zh_cn'
-uci set luci.main.mediaurlbase='/luci-static/argon'
+uci set luci.main.mediaurlbase='/luci-static/aurora'
 uci commit luci
 exit 0
